bbde57d72bb2cb9a9d56879bf015eb0ed8962905
Gavin Mackintosh <gmac685@aucklanduni.ac.nz>
2021-10-02 16:02:17 +1300

    Changed window siz

--- SpellingWiz/src/application/Main.java
+++ SpellingWiz/src/application/Main.java
@@ -11,9 +11,9 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		// loads and displays the main menu to the user
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
 			primaryStage.setTitle("Kēmu Kupu"); // set a title for the application
-			Scene scene = new Scene(root,700,700);
+			Scene scene = new Scene(root,1900,900);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false); // fix the size of GUI
