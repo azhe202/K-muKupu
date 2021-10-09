ffd9d84a375fcec32f33e554f84b4ee7184d5ac1
Gavin Mackintosh <gmac685@aucklanduni.ac.nz>
2021-10-09 20:34:20 +1300

    Added encouraging messages for the user

--- SpellingWiz/src/application/RewardScreen.java
+++ SpellingWiz/src/application/RewardScreen.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -10,6 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -27,22 +29,31 @@ public class RewardScreen implements Initializable{
 	private ImageView star4;
 	@FXML
 	private ImageView star5;
+	@FXML
+	private Label messageLabel;
+	@FXML
+	private ImageView nextPage;
 
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
 
 	public void start() {
+		messageLabel.setText("Unlucky, maybe try again to practice \n your spelling.");
+		
 		if (GamesModule.score>=1) {
-			star1.setImage(new Image("./download.png"));
+			star1.setImage(new Image("./filledStar.png"));
 		} if (GamesModule.score>=2) {
-			star2.setImage(new Image("./download.png"));
+			messageLabel.setText("Good job, you should try giving it \nanother go and see if you can \nget a higher score!");
+			star2.setImage(new Image("./filledStar.png"));
 		} if (GamesModule.score>=3) {
-			star3.setImage(new Image("./download.png"));
+			star3.setImage(new Image("./filledStar.png"));
 		} if (GamesModule.score>=4) {
-			star4.setImage(new Image("./download.png"));
-		} if (GamesModule.score>=5) {
-			star5.setImage(new Image("./download.png"));
+			messageLabel.setText("Great job! You should try \nanother word list now.");
+			star4.setImage(new Image("./filledStar.png"));
+		} if (GamesModule.score==5) {
+			messageLabel.setText("Awesome! Try another word list\n and see if you can get \nfull marks in that too!");
+			star5.setImage(new Image("./filledStar.png"));
 		}
 	}
 	
@@ -58,6 +69,15 @@ public class RewardScreen implements Initializable{
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		start();
 		
+	}
+	
+	public void exitNextPage(MouseEvent event) { 
+		nextPage.setImage(new Image("./arrow.png"));
+	}
+
+	public void enterNextPage(MouseEvent event) throws MalformedURLException { 
+		nextPage.setImage(new Image("./arrowSelect.png"));
+		Sound.playSound("./switch.wav");
 	}
 	
 	
